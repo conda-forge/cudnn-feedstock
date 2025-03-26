08249067c1c4c6d76ef1bb4e3ff936969860370e
Billy Suh <bsuh@nvidia.com>
2025-03-25 22:08:22 -0700

    Folders already exist so check for that

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,14 +1,14 @@
-mkdir %LIBRARY_INC%
+if not exist %LIBRARY_INC% mkdir %LIBRARY_INC%
 if errorlevel 1 exit 1
 copy %SRC_DIR%\include\cudnn*.h %LIBRARY_INC%\
 if errorlevel 1 exit 1
 
-mkdir %LIBRARY_LIB%
+if not exist %LIBRARY_LIB% mkdir %LIBRARY_LIB%
 if errorlevel 1 exit 1
 copy %SRC_DIR%\lib\x64\cudnn*.lib %LIBRARY_LIB%\
 if errorlevel 1 exit 1
 
-mkdir %LIBRARY_BIN%
+if not exist %LIBRARY_BIN% mkdir %LIBRARY_BIN%
 if errorlevel 1 exit 1
 copy %SRC_DIR%\bin\cudnn*.dll %LIBRARY_BIN%\
 if errorlevel 1 exit 1
